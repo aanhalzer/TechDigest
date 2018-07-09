0bc63841700f7186b0b4b5ceeb4e350a47ebf768
Andres <andresanhalzer@hotmail.com>
2018-07-08 21:57:57 -0400

    Add project files.

---
+++ TechDigest.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechDigest.Web.Models;
+
+namespace TechDigest.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private tdigestdbEntities db = new tdigestdbEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Week(int id)
+        {
+            IEnumerable<News> list = from n in db.News where n.WeekId == id select n;
+            ViewBag.Week = id.ToString();
+            return View(list);
+        }
+    }
+}
---
+++ TechDigest.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TechDigest.Web.MvcApplication" Language="C#" %>
